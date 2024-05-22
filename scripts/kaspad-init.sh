96f708ed2d9c36fb73476d25bf4f413772f89d89
Parkers145 <44621168+Parkers145@users.noreply.github.com>
2024-05-21 21:39:25 -0500

    Update kaspad-init.sh

--- scripts/kaspad-init.sh
+++ scripts/kaspad-init.sh
@@ -1,26 +1,8 @@
 #!/bin/bash
 
-# Define the log file
-LOG_FILE="/var/log/kaspad_run.log"
-
-# Function to log messages
-log_message() {
-    echo "$(date +'%Y-%m-%d %H:%M:%S') - $1" | tee -a "$LOG_FILE"
-}
-
 # Change directory to /usr/local/bin
 cd /usr/local/bin
-if [ $? -ne 0 ]; then
-    log_message "ERROR: Failed to change directory to /usr/local/bin"
-    exit 1
-fi
-log_message "INFO: Changed directory to /usr/local/bin"
 
 # Run the kaspad command with the --utxoindex flag
 ./kaspad --utxoindex
-if [ $? -ne 0 ]; then
-    log_message "ERROR: Failed to execute ./kaspad --utxoindex"
-    exit 1
-fi
 
-log_message "INFO: kaspad executed successfully with --utxoindex"
